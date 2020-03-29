619a76d3342096c37d2d18ab49659dbc7ea208ee
KP Dorsey <dorsey_k17@hotmail.com>
2020-03-28 19:46:41 -0500

    Working bubble but not per requirement

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,79 +1,38 @@
+// Refer to the Plotly.js documentation (https://plot.ly/javascript/) when building the plots.
+
 // // Check connection to app.js
 console.log("Connected to app.js");
-
-// Refer to the Plotly.js documentation (https://plot.ly/javascript/) when building the plots.
 
 /*****************************************************/
 // 1. Use the D3 library to read in `samples.json`.
 const url = "data/samples.json";
-
 // Fetch the JSON data - Promise Pending
 const data = d3.json(url);
 // Log results
-// console.log("Data Promise: ", data);
+console.log("Data Promise: ", data);
 
 // Unpack uitilty to extract child elements
-function unpack(rows, index) {
-  return rows.map(function (row) {
-    return row[index];
-  });
-}
+// function unpack(rows, index) {
+//   return rows.map(function (row) {
+//     return row[index];
+//   });
+// }
 
 /*****************************************************/
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-// Will likely create a bar chart function that accepts 'id' to filter the dataset
-// Mannually enter the otu index for now
+// Display data on html
 data.then((selectedData) => {
   // Store index otu index
-  var selected_otu_index = 0;
+  let selected_otu_index = 0;
 
-  // Check importedData in console
-  // console.log("importedData: ", importedData);
-
-  // Store importedData to local variable
-  var localData = selectedData;
-
+  // Store importedData to local varable
+  let localData = selectedData;
   // Check localData in console
   console.log("localData for bar chart: ", localData);
 
-  // Store name at index
-  var selected_otu = localData.names[selected_otu_index];
-  // Check names in console
-  // console.log("names: ", names);// names:  940
-
-  /******* Use `sample_values` as the values for the bar chart. *******/
-  // Map to sample_values
-  var selected_sample_values = localData.samples[selected_otu_index].sample_values;
-  // Check in console
-  // console.log("selected_sample_values: ", selected_sample_values);
-  /**********************************************************************/
-
-  /******* Use `otu_ids` as the labels for the bar chart. *******/
-  // Map to otu_ids
-  var selected_otu_ids = localData.samples[selected_otu_index].otu_ids;
-  // Check in console
-  // console.log("selected_otu_ids: ", selected_otu_ids);
-  /**********************************************************************/
-
-  /******* Use `otu_labels` as the hovertext for the chart. *******/
-  // Map to otu_labels
-  var selected_otu_labels = localData.samples[selected_otu_index].otu_labels;
-  // Check in console
-  // console.log("selected_otu_labels: ", selected_otu_labels);
-  /**********************************************************************/
-
-  /**********************************************************************/
-  // play with localData
-  var testing = {
-    x: selected_sample_values,
-    y: selected_otu_ids,
-    text: selected_otu_labels
-  };
-  console.log("testing: ", testing);
   /**********************************************************************/
   // Prepare data for bargraph
-  var selected_sample = localData.samples[selected_otu_index];
+  let selected_sample = localData.samples[selected_otu_index];
   // Sort the data array using the greekSearchResults value
   selected_sample.sample_values.sort(function (a, b) {
     return parseFloat(b.sample_values) - parseFloat(a.sample_values);
@@ -85,90 +44,23 @@ data.then((selectedData) => {
   // Reverse the array due to Plotly's defaults
   selected_sample.sample_values = selected_sample.sample_values.reverse();
 
-  // Trace1 - Working (all data for sample) for the OTU Data
-  // var trace1 = {
-  //     x: selected_sample_values,
-  //     y: selected_otu_ids,
-  //     text: selected_otu_labels,
-  //     name: "OTU",
-  //     type: "bar",
-  //     orientation: "h"
-  // };
-
-  // Trace2 - Working; returns top ten samples BUT returns all ids and labels for selected otu
-  // var trace2 = {
-  //   x: selected_sample.sample_values,
-  //   y: selected_sample.otu_ids,
-  //   text: selected_sample.otu_labels,
-  //   name: "OTU",
-  //   type: "bar",
-  //   orientation: "h"
-  // };
-  // console.log("trace2: ", trace2);
-
-  // Trace3 - Working; returns top ten samples and all otu_ids only for selected otu
-  // ... BUT out_labels not working AND otu_ids are duplating (x2)
-  // var trace3 = {
-  //   x: selected_sample.sample_values,
-  //   y: selected_sample.sample_values.map(row => selected_sample.otu_ids),
-  //   text: selected_sample.sample_values.map(row => selected_sample.otu_labels),
-  //   name: "OTU",
-  //   type: "bar",
-  //   orientation: "h"
-  // };
-  // console.log("trace3: ", trace3);
-
-  // Print values and index
-  // selected_sample.sample_values.map((item, index) => {
-  //   console.log(`Item:  ${item} Index:  ${index}`);
-  // });
-
-  // Trace4 - Working; returns top ten samples and all otu_ids only for selected otu
-  // ... BUT out_labels not working AND otu_ids are duplating (x2)
-  // var trace4 = {
-  //   x: selected_sample.sample_values,
-  //   y: selected_sample.sample_values.map(row => 
-  //     selected_sample.otu_ids.map((item) => {
-  //       item;
-  //     })
-  //   ),
-  //   test1: selected_sample.sample_values.map(row => selected_sample.otu_ids),
-  //   test2: selected_sample.sample_values.map(row => 
-  //     // console.log(`Outer Row: ${row}`)
-  //     // console.log(`Outer otu: ${selected_sample.otu_ids[row]}`)
-  //     selected_sample.otu_ids.slice(0, 10)[0]
-
-  //     // selected_sample.otu_ids.map((item, index) => {
-  //     //   console.log(`Row: ${row} Item: ${item} Index: ${index}`);
-  //     // })
-  //   ),
-  //   text: selected_sample.sample_values.map(row => selected_sample.otu_labels),
-  //   name: "OTU",
-  //   type: "bar",
-  //   orientation: "h"
-  // };
-  // console.log("trace4: ", trace4);
-  // console.log("trace4.test2: ", trace4.test2[0]); // Yatzee!!!
-
-  // Trace5 - Working; returns top ten samples and all otu_ids only for selected otu
-  // ... otu_labels show on hover BUT otu_ids on y-axis still duplating (x2)
-  var trace5 = {
+  // Trace - Working but y-axis label still duplating (x2)
+  let bar_trace = {
     x: selected_sample.sample_values,
-    // y: selected_sample.otu_ids.map(row => row).slice(0, 10),
     y: selected_sample.sample_values.map(row => selected_sample.otu_ids),
     text: selected_sample.otu_labels.map(row => row).slice(0, 10),
     name: "OTU",
     type: "bar",
     orientation: "h"
   };
-  console.log("trace5: ", trace5);
+  console.log("bar_trace: ",bar_trace);
 
   // data
-  var chartData = [trace5];
+  let bar_chartData = [bar_trace];
 
   // Apply the group bar mode to the layout
-  var layout = {
-    title: `OTU results for Test Subject ${selected_otu}`,
+  let layout = {
+    title: `OTU results for Test Subject ${localData.names[selected_otu_index]}`,
     margin: {
       l: 100,
       r: 100,
@@ -178,28 +70,59 @@ data.then((selectedData) => {
   };
 
   // Render the plot to the div tag with id "plot"
-  Plotly.newPlot("bar", chartData, layout);
+  Plotly.newPlot("bar", bar_chartData, layout);
+
   /**********************************************************************/
-
-  /*****************************************************/
   // 3. Create a bubble chart that displays each sample.
-
-  // * Use `otu_ids` for the x values.
-
-  // * Use `sample_values` for the y values.
-
-  // * Use `sample_values` for the marker size.
-
-  // * Use `otu_ids` for the marker colors.
-
+  
+  
+  
+  
   // * Use `otu_labels` for the text values.
 
+  // Trace - Working but y-axis label still duplating (x2)
+  let bubble_trace = {
+    // * Use `otu_ids` for the x values.
+    x: selected_sample.sample_values,
+    // * Use `sample_values` for the y values.
+    y: selected_sample.sample_values.map(row => selected_sample.otu_ids),
+    text: selected_sample.otu_labels.map(row => row).slice(0, 10),
+    name: "OTU",
+    type: "scatter",
+    mode: "markers",
+    marker: {
+      // * Use `otu_ids` for the marker colors.
+      color: 'rgb(31, 119, 180)',
+      // * Use `sample_values` for the marker size.
+      size: 18,
+      symbol: 'circle'
+    },
+    orientation: "h"
+  };
+  console.log("bubble_trace: ", bubble_trace);
+
+  // data
+  let bubble_chartData = [bubble_trace];
+
+  // Apply the group bar mode to the layout
+  // let layout = {
+  //   title: `OTU results for Test Subject ${localData.names[selected_otu_index]}`,
+  //   margin: {
+  //     l: 100,
+  //     r: 100,
+  //     t: 100,
+  //     b: 100
+  //   }
+  // };
+
+  // Render the plot to the div tag with id bubble""
+  Plotly.newPlot("bubble", bubble_chartData, layout);
 
   /*****************************************************/
   // 4. Display the sample metadata, i.e., an individual's demographic information.
   // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
   // Store metadata at index
-  var selected_otu_metadata = localData.metadata[selected_otu_index];
+  let selected_otu_metadata = localData.metadata[selected_otu_index];
   // Check names in console
   console.log("selected_otu_metadata: ", selected_otu_metadata);
   // Check value returned
@@ -220,7 +143,7 @@ data.then((selectedData) => {
   // Test key 'getKey'
   console.log("metadata id key", getKey(selected_otu_metadata, ethnicity));
 
-  var item = d3.select(".panel-body");
+  let item = d3.select(".panel-body");
   item.html("");
 
   item.append("p").text(`${getKey(selected_otu_metadata, id)}: ${id}`);
@@ -250,9 +173,9 @@ data.then((selectedData) => {
   
   
   // localData.forEach((selected_otu_metadata) => {
-  //   var row = sample_metadata.append("div");
+  //   let row = sample_metadata.append("div");
   //   Object.entries(selected_otu_metadata).forEach(([key, value]) => {
-  //     var cell = row.append("p");
+  //     let cell = row.append("p");
   //     cell.text(key, value);
   //   });
   // });
@@ -274,14 +197,14 @@ data.then((selectedData) => {
 // function optionChanged() {
 
     // // * Use `sample_values` as the values for the bar chart.
-    // var dropdownMenu = d3.selectAll("#selDataset").node();
-    // // Assign the value of the dropdown menu option to a variable
-    // // var dataset = dropdownMenu.property("onchange");
+    // let dropdownMenu = d3.selectAll("#selDataset").node();
+    // // Assign the value of the dropdown menu option to a varable
+    // // let dataset = dropdownMenu.property("onchange");
 
-    // // Assign the dropdown menu item ID to a variable
-    // var dropdownMenuID = dropdownMenu.id;
-    // // Assign the dropdown menu option to a variable
-    // var selectedOption = dropdownMenu.onchange;
+    // // Assign the dropdown menu item ID to a varable
+    // let dropdownMenuID = dropdownMenu.id;
+    // // Assign the dropdown menu option to a varable
+    // let selectedOption = dropdownMenu.onchange;
 
     // console.log("dropdownMenuID", dropdownMenuID);
     // console.log("selectedOption", selectedOption);
@@ -289,20 +212,20 @@ data.then((selectedData) => {
 
 // }
 
-// var dropdownMenu = d3.selectAll("#selDataset").node();
-// // Assign the value of the dropdown menu option to a variable
-// // var dataset = dropdownMenu.property("onchange");
+// let dropdownMenu = d3.selectAll("#selDataset").node();
+// // Assign the value of the dropdown menu option to a varable
+// // let dataset = dropdownMenu.property("onchange");
 
-// // Assign the dropdown menu item ID to a variable
-// var dropdownMenuID = dropdownMenu.id;
-// // Assign the dropdown menu option to a variable
-// var selectedOption = dropdownMenu.onchange;
+// // Assign the dropdown menu item ID to a varable
+// let dropdownMenuID = dropdownMenu.id;
+// // Assign the dropdown menu option to a varable
+// let selectedOption = dropdownMenu.onchange;
 
 // console.log("dropdownMenuID", dropdownMenuID);
 // console.log("selectedOption", selectedOption);
 
 // // Map to sample_names (ids)
-// var sample_names = localData.names;
+// let sample_names = localData.names;
 // console.log("sample_names", sample_names);
 // console.log("sample_names.length", sample_names.length);
 
